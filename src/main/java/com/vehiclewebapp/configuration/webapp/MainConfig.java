360273a1bd795f7844737d9366d19c552ccd4c33
garvit <garv8tverma26@gmail.com>
2022-07-05 21:33:19 +0530

    Updated MainConfig.java

--- src/main/java/com/vehiclewebapp/configuration/webapp/MainConfig.java
+++ src/main/java/com/vehiclewebapp/configuration/webapp/MainConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.vehiclewebapp")
-public class MainConfig {
+public class MainConfig implements WebMvcConfigurer {
 
     @Bean
     public ViewResolver getViewResolver(){
@@ -18,6 +20,11 @@ public class MainConfig {
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
     }
 
 }
