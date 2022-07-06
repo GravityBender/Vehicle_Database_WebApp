4bb1f6816e0285e98de04b4252daa55cecf57894
garvit <garv8tverma26@gmail.com>
2022-07-06 12:53:28 +0530

    Updated the controller files

--- src/main/java/com/vehiclewebapp/controller/LoginController.java
+++ src/main/java/com/vehiclewebapp/controller/LoginController.java
@@ -2,12 +2,19 @@ package com.vehiclewebapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("loginPage")
+    @GetMapping("/loginPage")
     public String showLoginPage(){
         return "admin-login";
     }
+
+    @GetMapping("/access-denied")
+    public String showAccessDeniedPage(){
+        return "access-denied";
+    }
+
 }
--- src/main/java/com/vehiclewebapp/controller/VehicleController.java
+++ src/main/java/com/vehiclewebapp/controller/VehicleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VehicleController {
 
-    @GetMapping("list")
+    @GetMapping("/list")
     public String showVehicleList(){
         return "list-vehicles";
     }
