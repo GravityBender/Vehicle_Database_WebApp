50b30b70359e7391f6ddc8be9a097b078eec675f
garvit <garv8tverma26@gmail.com>
2022-07-06 14:41:58 +0530

    Added support to jdbc support for login

--- src/main/java/com/vehiclewebapp/configuration/security/SecurityConfig.java
+++ src/main/java/com/vehiclewebapp/configuration/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vehiclewebapp.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,47 +8,48 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Autowired
+    private DataSource securityDataSource;
+
+    // For quick testing
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService(){
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//        UserDetails user1 = users.username("John")
+//                .password("password1")
+//                .roles("Admin")
+//                .build();
+//
+//        UserDetails user2 = users.username("Susan")
+//                .password("password2")
+//                .roles("Admin")
+//                .build();
+//
+//
+//        return new InMemoryUserDetailsManager(user1, user2);
+//    }
+
+
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(){
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        UserDetails user1 = users.username("John")
-                .password("password1")
-                .roles("Admin")
-                .build();
-
-        UserDetails user2 = users.username("Susan")
-                .password("password2")
-                .roles("Admin")
-                .build();
-
-
-        return new InMemoryUserDetailsManager(user1, user2);
+    public UserDetailsManager users(){
+        return new JdbcUserDetailsManager(securityDataSource);
     }
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//        http.authorizeRequests()
-//                .antMatchers("/css/**").permitAll()
-//                .antMatchers("/loginPage").authenticated()
-//                .and()
-//                .formLogin()
-//                    .loginPage("/loginPage")
-//                    .loginProcessingUrl("/authenticateTheUser")
-//                    .permitAll()
-//                .and()
-//                .logout()
-//                    .logoutSuccessUrl("/")
-//                    .permitAll()
-//                .and()
-//                .exceptionHandling().accessDeniedPage("/access-denied");
 
         http.authorizeRequests()
 
--- src/main/java/com/vehiclewebapp/configuration/webapp/MainConfig.java
+++ src/main/java/com/vehiclewebapp/configuration/webapp/MainConfig.java
@@ -1,18 +1,63 @@
 package com.vehiclewebapp.configuration.webapp;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.vehiclewebapp")
+@PropertySource("classpath:persistence-mysql.properties")
 public class MainConfig implements WebMvcConfigurer {
+
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource securityDataSource() {
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException exc) {
+            exc.printStackTrace();
+        }
+
+//        logger.info("jdbc.url: " + env.getProperty("jdbc.url"));
+//        logger.info("jdbc.user: " + env.getProperty("jdbc.user"));
+
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+
+        assert propVal != null;
+        return Integer.parseInt(propVal);
+    }
+
 
     @Bean
     public ViewResolver getViewResolver(){
